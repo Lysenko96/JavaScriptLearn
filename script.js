6b594e683f70e18559fcf3c676325241ae2fcd1c
Anton Lysenko <anton.lys96@gmail.com>
2020-08-14 17:44:17 +0300

    javascript learn

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'
+alert("hello")
+var text = "text";
+console.log(text);
+let second = 5;
+console.log(second);
+const pi = 3.14;
+console.log(pi);
+console.log(1/0);
+console.log('a'*3);
+//console.log(d); code not execute after error line
+let s;
+console.log(s);
+console.log(null);
+
+let person = {
+    name: "John Cena",
+    age: 44
+}
+
+console.log(person["name"]);
+console.log(person.age);
+
+let arr = ['img.png', 'img.jpg']
+
+console.log(arr[0]);
+console.log(arr[1]);
