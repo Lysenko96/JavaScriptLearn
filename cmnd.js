cce584eb14a55d71b9b25fdc04732b3363aa2c2e
Anton Lysenko <anton.lys96@gmail.com>
2021-07-20 17:21:21 +0300

    subStr

--- cmnd.js
+++ cmnd.js
@@ -1,26 +1,26 @@
-console.time('FirstWay');
-var s = "xac";
-var s1 = "acbdxf";
-var s2 = "acbdxxf";
-var s3 = "xfacb";
-var arr1 = [s, s1, s2, s3];
+//console.time('FirstWay');
+var myArr = [];
+for(var r = 2; r < process.argv.length; r++){
+    myArr.push(process.argv[r]);
+}
+console.log(myArr);
 var arr2 = [];
 var arr3 = [];
 var arr4 = [];
 var myVar = "";
-for (var x = 0; x < arr1.length; x++) {
-    for (var m = 0; m < arr1[x].length; m++) { 
+for (var x = 0; x < myArr.length; x++) {
+    for (var m = 0; m < myArr[x].length; m++) { 
         var arr = [];
         var vl = "";
-        for (var i = m; i < arr1[x].length; i++) { 
-            vl += arr1[x][i]; 
+        for (var i = m; i < myArr[x].length; i++) { 
+            vl += myArr[x][i]; 
             arr.push(vl);
         }
        // console.log(arr);
-        for (var c = 0; c < arr1.length; c++) {
+        for (var c = 0; c < myArr.length; c++) {
             for (var d = 0; d < arr.length; d++) {
                 if (c != x) {
-                    let res = arr1.every(function (e) {
+                    let res = myArr.every(function (e) {
                         return e.includes(arr[d]);
                     });
                     if (res) {
@@ -31,8 +31,6 @@ for (var x = 0; x < arr1.length; x++) {
         }
     }
 }
-//console.log(arr2);
-//console.log(arr3);
 var numbers = [];
 for (var z = 0; z < arr3.length; z++) {
     numbers.push(arr3[z].length);
@@ -44,6 +42,9 @@ for (var z = 0; z < arr3.length; z++) {
     if (arr3[z].length == sign) {
         console.log(arr3[z]);
         break;
-    }
+    } 
 }
-console.timeEnd('FirstWay');
+if( arr3[z] == undefined){
+    console.log();
+}
+//console.timeEnd('FirstWay');
