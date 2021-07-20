44db52df5b292e04b18b177005ddc13545d79543
Anton Lysenko <anton.lys96@gmail.com>
2021-07-20 17:35:43 +0300

    subStrProd

---
+++ lcs.js
@@ -0,0 +1,48 @@
+var myArr = [];
+for(var r = 2; r < process.argv.length; r++){
+    myArr.push(process.argv[r]);
+}
+var arr2 = [];
+var arr3 = [];
+var arr4 = [];
+var myVar = "";
+for (var x = 0; x < myArr.length; x++) {
+    for (var m = 0; m < myArr[x].length; m++) { 
+        var arr = [];
+        var vl = "";
+        for (var i = m; i < myArr[x].length; i++) { 
+            vl += myArr[x][i]; 
+            arr.push(vl);
+        }
+        for (var c = 0; c < myArr.length; c++) {
+            for (var d = 0; d < arr.length; d++) {
+                if (c != x) {
+                    let res = myArr.every(function (e) {
+                        return e.includes(arr[d]);
+                    });
+                    if (res) {
+                        arr3.push(arr[d]);
+                    }
+                }
+            }
+        }
+    }
+}
+var numbers = [];
+for (var z = 0; z < arr3.length; z++) {
+    numbers.push(arr3[z].length);
+}
+numbers.sort();
+var sign = numbers[numbers.length - 1];
+
+for (var z = 0; z < arr3.length; z++) {
+    if (arr3[z].length == sign) {
+        console.log(arr3[z]);
+        break;
+    } 
+}
+if(myArr.length == 1){
+    console.log(myArr[0]);
+} else if( arr3[z] == undefined){
+    console.log();
+}
