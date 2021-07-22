f293ce0a4c7212d5803b59a7c9c33b05560b6a93
Anton Lysenko <anton.lys96@gmail.com>
2021-07-22 19:01:20 +0300

    task2

--- task2.js
+++ task2.js
@@ -3,7 +3,8 @@ const sha3_256 = require('js-sha3').sha3_256;
 var mySet = [];
 fs.readdirSync(process.cwd(), "utf8").forEach(fileName => {
     if (checkIsFile(fileName)) {
-        console.log(fileName + " " + sha3_256(fs.readFileSync(fileName, "utf8")));
+        console.log(fileName + " " + sha3_256(fs.readFileSync(fileName, "utf8")
+            .replace("\n", "")));
         mySet.push(fileName)
     }
 });
